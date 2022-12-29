8639aca96e8b3989ea9ee07550583dd0d8bd570e
Joaquin Rodriguez <joaquin.rodriguez@live.com>
2022-12-29 11:41:33 -0300

    actualizaciones proyecto

--- src/App.js
+++ src/App.js
@@ -11,6 +11,22 @@ function App() {
     backgroundColor: 'blue'
   }
 
+  const cards = [
+    {
+      titulo:"FIFA WORD CUP",
+        descripcion:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quae consectetur, totam temporibus incidunt commodi",
+        img:"https://img.lagaceta.com.ar/fotos/notas/2022/10/11/copa-mundo-actual-se-estreno-mundial-alemania-1974-964951-113135.jpg",
+        btnText:"Ver Más",
+        btnClassName:"btn btn-danger fs-1"
+    },
+    {
+      titulo:"FIFA WORD CUP",
+        descripcion:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quae consectetur, totam temporibus incidunt commodi",
+        img:"https://caracoltv.brightspotcdn.com/dims4/default/3afdd34/2147483647/strip/true/crop/2048x1366+0+0/resize/1000x667!/quality/90/?url=http%3A%2F%2Fcaracol-brightspot.s3.amazonaws.com%2F39%2Ff0%2F4b22259e409d9eb185d79fdefa2d%2Ftrofeo-champions.jpg",
+        btnText:"Eliminar"
+    }
+  ]
+
 
   return (
     <div>
@@ -31,18 +47,29 @@ function App() {
 
         </a>
       </header>
-      <Card
-        titulo={"Hola Soy Componente"}
+      { cards.map(({titulo, descripcion, img, btnText, btnClassName}, index) => (
+            <Card 
+            key={index}
+            titulo={titulo}
+            descripcion={descripcion}
+            img={img}
+            btnText={btnText}
+            btnClassName={btnClassName}
+            />)
+      ) }
+      {/*<Card
+        titulo={"FIFA WORD CUP"}
         descripcion={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quae consectetur, totam temporibus incidunt commodi"}
         img={"https://img.lagaceta.com.ar/fotos/notas/2022/10/11/copa-mundo-actual-se-estreno-mundial-alemania-1974-964951-113135.jpg"}
         btnText={"Ver Más"}
+        btnClassName="btn btn-danger fs-1"
       />
-      <Card
-        titulo={"Componente Dos"}
+        <Card
+        titulo={"CHAMPIONS LEAGUE"}
         descripcion={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quae consectetur, totam temporibus incidunt commodi"}
-        img={"https://img.lagaceta.com.ar/fotos/notas/2022/10/11/copa-mundo-actual-se-estreno-mundial-alemania-1974-964951-113135.jpg"}
-        btnText={"Ver Más"}
-      />
+        img={"https://caracoltv.brightspotcdn.com/dims4/default/3afdd34/2147483647/strip/true/crop/2048x1366+0+0/resize/1000x667!/quality/90/?url=http%3A%2F%2Fcaracol-brightspot.s3.amazonaws.com%2F39%2Ff0%2F4b22259e409d9eb185d79fdefa2d%2Ftrofeo-champions.jpg"}
+        btnText={"Eliminar"}
+      />*/}
     </div>
   );
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
 const Card = (props) => {
-  const {titulo, descripcion,img,btnText} = props //es un objeto
+  const {titulo, descripcion,img,btnText,btnClassName} = props //es un objeto
   return (
-    <div className="card" style={{width: "18rem;"}}>
-  <img src={img} className="card-img-top" alt="..."/>
+    <div className="card" style={{width: "18rem"}}>
+  <img src={img} className="card-img-top" alt="..." />
   <div className="card-body">
     <h5 className="card-title">{titulo}</h5>
     <p className="card-text">{descripcion}</p>
-    <a href="#" className="btn btn-primary">{btnText}</a>
+    <a href="#" className={ btnClassName || "btn btn-primary"}>
+      {btnText}
+    </a>
   </div>
 </div>
   )
