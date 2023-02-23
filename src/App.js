a708160f9ae1273cf4a45a5a29eb8f9c9a83513e
Joaquin Rodriguez <joaquin.rodriguez@live.com>
2023-02-23 03:55:32 -0300

    context

--- src/App.js
+++ src/App.js
@@ -1,47 +1,13 @@
-import { useState } from "react"
+import ItemListContainer from "./components/ItemListContainer";
+import Navbar from "./components/Navbar";
+import CarritoContextProvider from "./context/CarritoContext";
 
 function App() {
-
-  const [state, setState]= useState('')
-  const vocales = ['a','e','i','o','u']
-
-  const handleClick = (evnt) => {
-    console.clear()
-    console.log(evnt)
-  } 
-
-  const handleSubmit = (e) =>  {
-
-    e.preventDefault();
-
-    console.log("se ejecuto el handleSubmit")
-  }
-
-  const handleKeyDown = (e) => {
-
-    const {key} = e
-
-      if(!vocales.includes(key.toLowerCase())){
-        setState(key)
-      }
-
-  }
-
   return (
-    <>
-    <div>
-        <h1>Hola Mundo ! </h1>
-        <form onSubmit={handleSubmit} action="">
-          <input value={state} onKeyDown={handleKeyDown} type="text"/>
-          <button type="submit">Guardar el Form </button>
-        </form>
-        <button onClick={handleClick}>Clickea me</button>
-    </div>
-    <div>
-      
-    </div>
-    </>
-
+    <CarritoContextProvider>
+      <Navbar/>
+      <ItemListContainer/>
+    </CarritoContextProvider>
   );
 }
 
